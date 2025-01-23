f5d7b8ede1118b7b9be97425a331467c6282b3cb
MyllenaSales <myllenasales50@gmail.com>
2025-01-23 15:23:32 -0300

    commit

--- src/main/java/br/edu/ifba/demo/backend/api/repository/CadastroRepository.java
+++ src/main/java/br/edu/ifba/demo/backend/api/repository/CadastroRepository.java
@@ -1,5 +1,17 @@
 package br.edu.ifba.demo.backend.api.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+import br.edu.ifba.demo.backend.api.model.CadastroModel;
+import org.springframework.stereotype.Repository;
 
-public class CadastroRepository {
-    
+@Repository
+public interface CadastroRepository 
+	extends JpaRepository<CadastroModel, Long>
+{
+	Optional<CadastroModel> findById(Long idCadastro);
+    Optional<CadastroModel> findByNome(String nomeCadastro);
+    Optional<CadastroModel> findByCpf(String cpfCadastro);
+    Optional<CadastroModel> findByEmail(String emailCadastro);
+
+
 }

d13f5d4c0b2eb281bdd1af0fea5d5f545af5e390
MyllenaSales <myllenasales50@gmail.com>
2025-01-27 12:13:21 -0300

    commit

--- src/main/java/br/edu/ifba/demo/backend/api/controller/CadastroController.java
+++ src/main/java/br/edu/ifba/demo/backend/api/controller/CadastroController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-@RequestMapping("/Cadastro")
+@RequestMapping("/cadastro")
 public class CadastroController {
 
     private final CadastroRepository cadastroRepository;
